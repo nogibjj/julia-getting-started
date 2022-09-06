9c57983594443e5980e7af5250c0b76b41c4699c
Noah Gift <noah.gift@gmail.com>
2022-09-06 18:58:35 +0000

    adding julia cli

---
+++ cliJuliaParse.jl
@@ -0,0 +1,39 @@
+#!/usr/bin/env julia
+# Path: cliJuliaParse.jl
+# Compare this snippet from cliJulia.jl:
+
+using ArgParse
+
+## Kick the tires with copilot next time and tell it to build functions
+
+
+function parse_commandline()
+    s = ArgParseSettings()
+
+    @add_arg_table s begin
+        "--opt1"
+            help = "an option with an argument"
+        "--opt2", "-o"
+            help = "another option with an argument"
+            arg_type = Int
+            default = 0
+        "--flag1"
+            help = "an option without argument, i.e. a flag"
+            action = :store_true
+        "arg1"
+            help = "a positional argument"
+            required = true
+    end
+
+    return parse_args(s)
+end
+
+function main()
+    parsed_args = parse_commandline()
+    println("Parsed args:")
+    for (arg,val) in parsed_args
+        println("  $arg  =>  $val")
+    end
+end
+
+main()
